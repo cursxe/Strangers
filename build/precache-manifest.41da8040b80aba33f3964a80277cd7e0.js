d405a70f7ff3bf1eb3beebf150dbca1d2be12d0a
cursxe <102540471+cursxe@users.noreply.github.com>
2022-09-15 04:02:29 -0400

    Add files via upload

---
+++ build/precache-manifest.41da8040b80aba33f3964a80277cd7e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4698e33a9841d2b36d75",
+    "url": "/static/css/main.ff3852f0.chunk.css"
+  },
+  {
+    "revision": "4698e33a9841d2b36d75",
+    "url": "/static/js/main.4698e33a.chunk.js"
+  },
+  {
+    "revision": "bc224e09b3af61393ee6",
+    "url": "/static/js/1.bc224e09.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c5da135822ca12f192a00d268092373",
+    "url": "/index.html"
+  }
+];
